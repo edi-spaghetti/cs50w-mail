7719e3e34dc21d265d9dcca358f2b385a3cbb9c1
Edward Spencer <epfspencer@yahoo.com>
2021-01-26 23:12:32 +0000

    Convert to single quoted strings
    I'm sorry Brian, double quotes make me sad.

--- mail/models.py
+++ mail/models.py
@@ -7,9 +7,9 @@ class User(AbstractUser):
 
 
 class Email(models.Model):
-    user = models.ForeignKey("User", on_delete=models.CASCADE, related_name="emails")
-    sender = models.ForeignKey("User", on_delete=models.PROTECT, related_name="emails_sent")
-    recipients = models.ManyToManyField("User", related_name="emails_received")
+    user = models.ForeignKey('User', on_delete=models.CASCADE, related_name='emails')
+    sender = models.ForeignKey('User', on_delete=models.PROTECT, related_name='emails_sent')
+    recipients = models.ManyToManyField('User', related_name='emails_received')
     subject = models.CharField(max_length=255)
     body = models.TextField(blank=True)
     timestamp = models.DateTimeField(auto_now_add=True)
@@ -18,12 +18,12 @@ class Email(models.Model):
 
     def serialize(self):
         return {
-            "id": self.id,
-            "sender": self.sender.email,
-            "recipients": [user.email for user in self.recipients.all()],
-            "subject": self.subject,
-            "body": self.body,
-            "timestamp": self.timestamp.strftime("%b %-d %Y, %-I:%M %p"),
-            "read": self.read,
-            "archived": self.archived
+            'id': self.id,
+            'sender': self.sender.email,
+            'recipients': [user.email for user in self.recipients.all()],
+            'subject': self.subject,
+            'body': self.body,
+            'timestamp': self.timestamp.strftime('%b %-d %Y, %-I:%M %p'),
+            'read': self.read,
+            'archived': self.archived
         }
